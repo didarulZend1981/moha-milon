02a285b9ed50f04f2db11e52fa05fb1522786345
didarulZend1981exitgit config --global user.name didarulZend1981git config --global user.name didarulZend1981 <didarul1981@gmail.com>
2024-04-02 02:51:55 +0600

    Loding--7 Introduction To Private Route And Handle Loading State

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -7,21 +7,27 @@ export const AuthContext =createContext(null);
 
 const AuthProvider = ({children}) => {
   const [user, setUser]= useState(null);
+  const [loading,setLoading] = useState(true);
+
   const createUser = (email, password) =>{
+    setLoading(true);
     return createUserWithEmailAndPassword(auth,email,password);
   }
   const singInUser =(email, password)=>{
+    setLoading(true);
     return signInWithEmailAndPassword(auth,email ,password)
   
   }
   // const authInfo = { user,createUser}
 
   const logOut = () =>{
+    setLoading(true);
     return signOut(auth);
   }
   useEffect(()=>{
     const unSubscribe= onAuthStateChanged(auth,currentUser=>{
       setUser(currentUser);
+      setLoading(false);
       console.log('observeing current user',currentUser)
     });
 
@@ -36,7 +42,8 @@ const AuthProvider = ({children}) => {
     user,
     createUser,
     singInUser,
-    logOut
+    logOut,
+    loading
   
   }
 
